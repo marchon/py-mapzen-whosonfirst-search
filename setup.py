0530c61a1e02a0e6d4b9a8cc4093c13bbdd93f16
thisisaaronland <aaron@mapzen.com>
2015-09-22 17:23:35 +0000

    pass along timeout flags from CLI apps

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ desc = open("README.md").read(),
 setup(
     name='mapzen.whosonfirst.search',
     namespace_packages=['mapzen', 'mapzen.whosonfirst', 'mapzen.whosonfirst.search'],
-    version='0.03',
+    version='0.04',
     description='Simple Python wrapper for Who\'s On First search functionality',
     author='Mapzen',
     url='https://github.com/mapzen/py-mapzen-whosonfirst-search',
@@ -24,5 +24,5 @@ setup(
         'scripts/wof-es-index-files',
         'scripts/wof-es-prepare',
         ],
-    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-search/releases/tag/v0.03',
+    download_url='https://github.com/mapzen/py-mapzen-whosonfirst-search/releases/tag/v0.04',
     license='BSD')
